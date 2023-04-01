dc0a46be2e26ac49c2bd4f57629f91df2bdb745f
Angad <anilterapalli@gmail.com>
2023-04-01 21:35:29 +0530

    s1

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -13,7 +13,7 @@ public class Message {
     public Message(int id, String content, Date timestamp) {
         this.id = id;
         this.content = content;
-        this.timestamp = timestamp;
+        this.timestamp = new Date();
     }
 
     public int getId() {
